f7faece1bfb708931c641c397e308bd801d6b8c6
Roy Nieterau <roy_nieterau@hotmail.com>
2026-05-02 17:25:16 +0200

    Removing instances should directly disappear from the UI.

--- client/ayon_comfyui/plugins/create/create_model.py
+++ client/ayon_comfyui/plugins/create/create_model.py
@@ -145,6 +145,8 @@ class ModelCreator(Creator):
             [i.data_to_store() for i in instances], PublishType.MODEL3D
         )
         stub.remove_instance(instances)
+        for instance in instances:
+            self._remove_instance_from_context(instance)
 
     def get_pre_create_attr_defs(self):
         return [
